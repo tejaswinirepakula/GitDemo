92e69ac0b8055e96989db69c0383ca486399dd13
tejaswinir11 <tejaswinir11@gmail.com>
2019-12-19 11:47:03 +0530

    American guy commit

--- src/test/java/com/teja/app/RestApITest.java
+++ src/test/java/com/teja/app/RestApITest.java
@@ -8,6 +8,11 @@ public class RestApITest {
 		System.out.println("postJira");
 		System.out.println("postJira1");
 		System.out.println("postJira2");
+		System.out.println("postJira3");
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
 	}
 	@Test
 	public void deleteTwitter() {
