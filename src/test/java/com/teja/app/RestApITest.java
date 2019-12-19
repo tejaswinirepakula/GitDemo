283e84f100ae338b5d100fde7a8bdbc4d7ca97bb
tejaswinir11 <tejaswinir11@gmail.com>
2019-12-19 11:39:00 +0530

    this ic commit from Asian Timezone

--- src/test/java/com/teja/app/RestApITest.java
+++ src/test/java/com/teja/app/RestApITest.java
@@ -6,6 +6,8 @@ public class RestApITest {
 	@Test
 	public void postJira() {
 		System.out.println("postJira");
+		System.out.println("postJira1");
+		System.out.println("postJira2");
 	}
 	@Test
 	public void deleteTwitter() {
